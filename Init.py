d259e612850896f7cdafc5521716f49247f0dbb9
Ashby Wood <ashby.wood@gmail.com>
2026-01-14 23:30:11 -0700

    Fifth commit

--- Init.py
+++ Init.py
@@ -3,3 +3,4 @@ print("This is my second change to the Init.py file.")
 print("Learning git is tricky but I often misspell words which is frustrating.")
 
 #Thinking of something reandom to type is tricky
+print("It is interesting how VS Code knows what I am going to type next sometimes.")
