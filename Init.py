eb8aa7ea0bae2f61708825487e5e442b45970911
Ashby Wood <ashby.wood@gmail.com>
2026-01-14 23:27:07 -0700

    Fourth commit, added a new comment

--- Init.py
+++ Init.py
@@ -1,3 +1,5 @@
 #Working with git and Python
 print("This is my second change to the Init.py file.")
 print("Learning git is tricky but I often misspell words which is frustrating.")
+
+#Thinking of something reandom to type is tricky
